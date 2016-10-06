9fba7de6c7dbe4119bebbd0574881cf60f2304ed
Sami Al-Jamal <sami.m.aljamal@gmail.com>
2016-10-05 23:11:42 -0700

    Create teh basic user authentication functionality

---
+++ src/AroundTown3/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AroundTown3.ViewModels
+{
+    public class LoginViewModel
+    {
+            public string Email { get; set; }
+            public string Password { get; set; }
+    }
+}
